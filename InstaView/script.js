2bae63b7ae02628770047990ec95fab9a3967f32
Jay Solanki <jaysol8900@gmail.com>
2025-04-23 10:56:33 +0530

    InstaView

---
+++ InstaView/script.js
@@ -0,0 +1,40 @@
+const storyboard = [
+    { "dp": "https://images.unsplash.com/photo-1710410798307-f8e4efc26734?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mjh8fGJveXMlMjBtb2RlbHxlbnwwfHwwfHx8MA%3D%3D", "story": "https://images.unsplash.com/photo-1710410798205-bfd583c9c156?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mjd8fGJveXMlMjBtb2RlbHxlbnwwfHwwfHx8MA%3D%3D" },
+    { "dp": "https://images.unsplash.com/photo-1652278859457-837e5884de31?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fGJveXMlMjBtb2RlbHxlbnwwfHwwfHx8MA%3D%3D", "story": "https://images.unsplash.com/photo-1652278858296-ba17fe705ddd?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fGJveXMlMjBtb2RlbHxlbnwwfHwwfHx8MA%3D%3D" },
+    { "dp": "https://images.unsplash.com/photo-1582061086805-8df3b58a94b9?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fGJveXMlMjBtb2RlbHxlbnwwfHwwfHx8MA%3D%3D", "story": "https://images.unsplash.com/photo-1652278858177-6c3cf833071e?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjJ8fGJveXMlMjBtb2RlbHxlbnwwfHwwfHx8MA%3D%3D" },
+    { "dp": "https://plus.unsplash.com/premium_photo-1706806943425-e5cc742f62e0?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDl8fGJveXMlMjBtb2RlbHxlbnwwfHwwfHx8MA%3D%3D", "story": "https://images.unsplash.com/photo-1652278859144-bded86ce912e?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Njh8fGJveXMlMjBtb2RlbHxlbnwwfHwwfHx8MA%3D%3D" },
+    { "dp": "https://plus.unsplash.com/premium_photo-1727193170815-c2ae9984c26d?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Njl8fGRlc3RpbmF0aW9ufGVufDB8fDB8fHww", "story": "https://images.unsplash.com/photo-1605152912625-6c4c2b32aec8?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzR8fGRlc3RpbmF0aW9ufGVufDB8fDB8fHww" },
+    { "dp": "https://plus.unsplash.com/premium_photo-1661962660197-6c2430fb49a6?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8ZGVzdGluYXRpb258ZW58MHx8MHx8fDA%3D", "story": "https://images.unsplash.com/photo-1682687221038-404cb8830901?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mzl8fGRlc3RpbmF0aW9ufGVufDB8fDB8fHww" },
+    { "dp": "https://images.unsplash.com/photo-1682687982183-c2937a74257c?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8ZGVzdGluYXRpb258ZW58MHx8MHx8fDA%3D", "story": "https://images.unsplash.com/photo-1507062147571-a3612422cd91?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDd8fGRlc3RpbmF0aW9ufGVufDB8fDB8fHww" },
+    { "dp": "https://plus.unsplash.com/premium_photo-1663957821802-4969fe6a0347?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTN8fHBob3RvZ3JhcGh5fGVufDB8fDB8fHww", "story": "https://images.unsplash.com/photo-1578010505898-c47ab4c85775?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDR8fGRlc3RpbmF0aW9ufGVufDB8fDB8fHww" },
+    { "dp": "https://images.unsplash.com/photo-1548502499-ef49e8cf98d4?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fHBob3RvZ3JhcGh5fGVufDB8fDB8fHww", "story": "https://images.unsplash.com/photo-1527011046414-4781f1f94f8c?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fHBob3RvZ3JhcGh5fGVufDB8fDB8fHww" },
+    { "dp": "https://images.unsplash.com/photo-1506241537529-eefea1fbe44d?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fHBob3RvZ3JhcGh5fGVufDB8fDB8fHww", "story": "https://images.unsplash.com/photo-1554080353-a576cf803bda?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fHBob3RvZ3JhcGh5fGVufDB8fDB8fHww" }
+  ];
+
+var storycontainer = document.querySelector(".storycontainer");
+
+var storyimage = document.querySelector(".storyimage");
+
+storyboard.forEach((element, index) => {
+    storycontainer.innerHTML += `<div  class="story"><img id="${index}" src="${element.dp}" alt="dp"></div>`;
+});
+
+document.querySelectorAll(".story").forEach((story) => {
+    story.addEventListener("click", (e) => {
+        var storyid=e.target.id;
+        var storyimagelink = storyboard[storyid].story; 
+        storyimage.children[0].src = storyimagelink;
+        storyimage.style.display = "block";
+        
+
+        setTimeout(() => {
+            storyimage.style.display = "none";
+        }, 3000);
+    });
+});
+
+
+
+
+
+
